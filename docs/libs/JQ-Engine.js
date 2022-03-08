7b25ea5b23202c5e833dabec5ade7b4e3b601d14
Bartosz Jażdżyk <bartek.jazdzyk.007@gmail.com>
2022-03-08 19:37:46 +0100

    prevent ctrl +/-

--- docs/libs/JQ-Engine.js
+++ docs/libs/JQ-Engine.js
@@ -9,7 +9,11 @@ $("head").append(`<style>
 	image-rendering: pixelated;
 }
 </style>`)
-
+$("body").keydown(function(event) {
+	if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+		event.preventDefault();
+	}
+});
 
 const keys = {}
 const listeners = {}
